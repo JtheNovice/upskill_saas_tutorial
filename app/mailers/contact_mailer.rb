a6e7effc861b7f2cce555298461304db5785c9fb
The Novice <jaythenovice@proton.me>
2022-08-23 14:18:03 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'jaythenovice@proto.me'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
